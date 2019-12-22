7e13d709d451acfbe8ed462505b603da0d0c3ae1
Ali Jarjis <ajarjis@hotmail.co.uk>
2019-12-22 19:57:21 +0000

    Added modal

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,6 +3,6 @@ $("#contactForm").submit(function(e) {
 
   var $form = $(this);
   $.post($form.attr("action"), $form.serialize()).then(function() {
-    alert("Thank you for your message. Your enquiry is important to us, we will be in touch with you shortly.");
+    $('#successMessage').modal('show');
   });
 });
