6ba920b6ec8654bcafe32f04f7e5524ef9c5e699
Ali Jarjis <ajarjis@hotmail.co.uk>
2019-12-22 19:59:38 +0000

    Minor

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -4,5 +4,6 @@ $("#contactForm").submit(function(e) {
   var $form = $(this);
   $.post($form.attr("action"), $form.serialize()).then(function() {
     $('#successMessage').modal('show');
+    $('#contactForm').trigger("reset");
   });
 });
